64a6e12a8a51e5b04b8078be05afe9f8fb09bcf0
Иван Скиба <iskiba60@gmail.com>
2024-07-18 18:39:44 +0300

    Правки - создал новый метод и поместил в него запуск задач, также метод запускается в отдельном потоке, что не мешает добавлять в очередь новые задания

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
-import time
-
-import mp_queue
-
-
-def example_function(duration: int, message: str) -> None:
-    """Пример фунции."""
-    start_time = time.time()
-    print(f'Запуск задачи - {message}.')
-    time.sleep(duration)
-    ex_time = time.time() - start_time
-    print(f'Задача {message} завершила работу за {ex_time:.0f}.')
+from mp_queue import ProcessController, example_function
 
 
 if __name__ == '__main__':
-    controller = mp_queue.ProcessController()
+    controller = ProcessController()
 
     # Ограничение на максимальное число одновременно выполняемых процессов
     controller.set_max_proc(2)
 
     # Список заданий
     tasks = [
-        (example_function, (2, 'Task 1')),
-        (example_function, (3, 'Task 2')),
-        (example_function, (4, 'Task 3')),
-        (example_function, (5, 'Task 4')),
-        (example_function, (6, 'Task 5')),
-        (example_function, (7, 'Task 6')),
-        (example_function, (8, 'Task 7')),
+        (example_function, (3, 'Task 1')),
+        (example_function, (4, 'Task 2')),
+        (example_function, (5, 'Task 3')),
+        (example_function, (6, 'Task 4')),
+        (example_function, (7, 'Task 5')),
+    ]
+
+    # Второй список заданий
+    second_tasks = [
+        (example_function, (1, 'Task 6')),
+        (example_function, (2, 'Task 7')),
+        (example_function, (3, 'Task 8')),
+        (example_function, (4, 'Task 9')),
     ]
 
     # Максимальное время выполнения 5 секунд
     controller.start(tasks, max_exec_time=5)
+
+    print(f'Осталось задач: {controller.wait_count()}')
+    print(f'Запущенные задачи: {controller.alive_count()}')
+
+    print('Запускается второй вызов')
+
+    # Максимальное время выполнения 5 секунд
+    controller.start(second_tasks, max_exec_time=3)
+
+    print(f'Осталось задач: {controller.wait_count()}')
+    print(f'Запущенные задачи: {controller.alive_count()}')
 
     # Ожидание завершения всех задач
     controller.wait()
--- mp_queue.py
+++ mp_queue.py
@@ -1,17 +1,18 @@
 import multiprocessing
+import threading
+import queue
 import time
-
-# TODO: прописать README.md
-# TODO: залить на гитхаб
 
 
 class ProcessController:
     def __init__(self):
         """Инициализация атрибутов."""
         self.max_procs = 1  # кол-во запущенных одновременно процессов
-        self.running_processes = []
-        self.queue = multiprocessing.Queue()
+        self.running_processes = {}
+        self.queue = queue.Queue()
         self.count_of_tasks = 0  # атрибут для подсчета количества задач
+        runner = threading.Thread(target=self._run_processes)
+        runner.start()
 
     def set_max_proc(self, n: int) -> None:
         """Устанавливает ограничение: максимальное число
@@ -24,33 +25,38 @@ class ProcessController:
         и запускает их параллельно.
         """
         for task in tasks:
-            self.queue.put(task)
+            self.queue.put((task, max_exec_time))
             self.count_of_tasks += 1
 
-        while not self.queue.empty() or self.running_processes:
-            for p, start_time in self.running_processes.copy():
+    def _run_processes(self):
+        while True:
+            proc_to_remove = []
+            for p, end_time in self.running_processes.items():
                 if not p.is_alive():
-                    self.running_processes.remove((p, start_time))
-                elif time.time() - start_time > max_exec_time:
+                    proc_to_remove.append(p)
+                elif end_time <= time.time():
                     p.terminate()
                     print(f'Процесс {p} завершился из-за ограничения.')
-                    self.running_processes.remove((p, start_time))
+                    proc_to_remove.append(p)
+
+            for p in proc_to_remove:
+                self.running_processes.pop(p)
+                self.queue.task_done()
 
             # Запуск новых процессов, если есть доступные слоты
             while (len(self.running_processes) < self.max_procs
                    and not self.queue.empty()):
-                func, args = self.queue.get()
+                (func, args), max_exec_time = self.queue.get()
                 self.count_of_tasks -= 1
                 p = multiprocessing.Process(target=func, args=args)
                 p.start()
-                self.running_processes.append((p, time.time()))
+                self.running_processes[p] = time.time() + max_exec_time
 
             time.sleep(0.1)
 
     def wait(self) -> None:
         """Ждет завершения выполнения всех заданий из очереди."""
-        for p in self.running_processes:
-            p.join()
+        self.queue.join()
 
         print('Все процессы завершили работу.')
 
@@ -61,3 +67,12 @@ class ProcessController:
     def alive_count(self) -> int:
         """Возвращает число выполняемых в данный момент заданий."""
         return len(self.running_processes)
+
+
+def example_function(duration: int, message: str) -> None:
+    """Пример фунции."""
+    start_time = time.time()
+    print(f'Запуск задачи - {message}.')
+    time.sleep(duration)
+    ex_time = time.time() - start_time
+    print(f'Задача {message} завершила работу за {ex_time:.0f} секунды.')
